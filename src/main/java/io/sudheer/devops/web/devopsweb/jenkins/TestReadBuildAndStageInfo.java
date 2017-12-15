421ec4333c6416a34f135276ee40791334d310d2
veersudhir83 <veersudhir83@gmail.com>
2017-12-15 20:39:51 +0530

    Using MultiBranchPipeline default for testing

--- src/main/java/io/sudheer/devops/web/devopsweb/jenkins/TestReadBuildAndStageInfo.java
+++ src/main/java/io/sudheer/devops/web/devopsweb/jenkins/TestReadBuildAndStageInfo.java
@@ -4,10 +4,10 @@ public class TestReadBuildAndStageInfo {
 
 	public static void main(String[] args) throws Exception {
 		String jenkinsURL = "http://192.168.43.115:8080";
-		String projectName = "Pipeline-devops-web-maven";
+		String projectName = "devops-web-hackathon";
 		String projectBranch = "master";
-		String buildNumber = "26";
-		boolean multiBranchPipeline = false;
+		String buildNumber = "4";
+		boolean multiBranchPipeline = true;
 		//URL for MultiBranch Configuration Pipelines
 		//String mainURL = "http://192.168.43.115:8080/job/devops-web-hackathon/job/master/4";
 		
