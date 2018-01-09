1fcb42178204d98c163f7ade833f0ec0257bff86
veersudhir83 <veersudhir83@gmail.com>
2018-01-09 21:13:18 +0530

    Writing jenkins data to CSV

--- src/main/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplication.java
+++ src/main/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplication.java
@@ -1,5 +1,9 @@
 package io.sudheer.devops.web.devopsweb;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +11,33 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import io.sudheer.devops.web.devopsweb.jenkins.ReadBuildAndStageInfo;
+import io.sudheer.devops.web.devopsweb.jenkins.utils.JobDetailsDAO;
+
 @EnableAutoConfiguration
 public class DevopsWebMavenApplication {
     public static void main (String[] args) {
+    		try {
+    			String jenkinsURL = "http://192.168.43.115:8080";
+    			String projectName = "devops-web-hackathon";
+    			String projectBranch = "master";
+    			boolean isMultiBranchPipeline = true;
+    			
+    			JobDetailsDAO jobDetailsObj = new JobDetailsDAO();
+    			jobDetailsObj.setJenkinsURL(jenkinsURL);
+    			jobDetailsObj.setProjectName(projectName);
+    			jobDetailsObj.setProjectBranch(projectBranch);
+    			jobDetailsObj.setIsMultiBranchPipeline(isMultiBranchPipeline);
+    			
+    			StringBuilder sb = ReadBuildAndStageInfo.getFullInfo(jobDetailsObj);
+    			File file = new File("data.csv");
+    			try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+    			    writer.write(sb.toString());
+    			}
+    			//System.out.println(ReadBuildAndStageInfo.getFullInfo(jobDetailsObj));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
         SpringApplication app = new SpringApplication(DevopsWebMavenApplication.class);
         app.run(args);
     }
