3f9a5ccadeb7785974a7905c5f2b16657f6284cc
veersudhir83 <veersudhir83@gmail.com>
2018-01-11 19:44:57 +0530

    Removed jenkins dashboard files from this project

--- src/main/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplication.java
+++ src/main/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplication.java
@@ -11,33 +11,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import io.sudheer.devops.web.devopsweb.jenkins.ReadBuildAndStageInfo;
-import io.sudheer.devops.web.devopsweb.jenkins.utils.JobDetailsDAO;
 
 @EnableAutoConfiguration
 public class DevopsWebMavenApplication {
     public static void main (String[] args) {
-    		try {
-    			String jenkinsURL = "http://192.168.43.115:8080";
-    			String projectName = "devops-web-hackathon";
-    			String projectBranch = "master";
-    			boolean isMultiBranchPipeline = true;
-    			
-    			JobDetailsDAO jobDetailsObj = new JobDetailsDAO();
-    			jobDetailsObj.setJenkinsURL(jenkinsURL);
-    			jobDetailsObj.setProjectName(projectName);
-    			jobDetailsObj.setProjectBranch(projectBranch);
-    			jobDetailsObj.setIsMultiBranchPipeline(isMultiBranchPipeline);
-    			
-    			StringBuilder sb = ReadBuildAndStageInfo.getFullInfo(jobDetailsObj);
-    			File file = new File("data.csv");
-    			try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-    			    writer.write(sb.toString());
-    			}
-    			//System.out.println(ReadBuildAndStageInfo.getFullInfo(jobDetailsObj));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
         SpringApplication app = new SpringApplication(DevopsWebMavenApplication.class);
         app.run(args);
     }
